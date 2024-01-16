35763fcba8850bf7bdefc3bb72233a26f7fe7808
Saúl Cabrera <saulecabrera@gmail.com>
2024-01-16 13:18:09 -0500

    Font

--- nixpkgs/.wezterm.lua
+++ nixpkgs/.wezterm.lua
@@ -9,7 +9,7 @@ if wezterm.config_builder then
 end
 
 config.font = wezterm.font('PragmataPro Mono Liga')
-config.font_size = 14
+config.font_size = 14.5
 
 config.color_scheme = 'Gruvbox dark, medium (base16)'
 return config
