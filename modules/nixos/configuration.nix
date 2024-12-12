90121ae9f580629c70831e70e6b5a811750635fd
Saúl Cabrera <saulecabrera@gmail.com>
2024-12-12 12:00:31 -0500

    More config

--- modules/nixos/configuration.nix
+++ modules/nixos/configuration.nix
@@ -17,6 +17,7 @@ in
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
+  boot.initrd.kernelModules = [ "amdgpu" ];
 
   networking.hostName = "nixos"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
@@ -49,15 +50,16 @@ in
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
-  # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 
+  services.xserver.videoDrivers = [ "modesetting" "displaylink" ];
   # Configure keymap in X11
   services.xserver.xkb = {
     layout = "us";
     variant = "altgr-intl";
   };
+  services.hardware.bolt.enable = true;
 
   # Enable CUPS to print documents.
   services.printing.enable = true;
