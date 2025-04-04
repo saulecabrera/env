5bcf844480b82e315fd55ae430f23c41cc8a64bf
Saúl Cabrera <saulecabrera@gmail.com>
2025-04-04 13:33:40 -0400

    Update

--- modules/shared/programs.nix
+++ modules/shared/programs.nix
@@ -91,7 +91,7 @@ in
     initExtra = ''
       [ -f /opt/dev/dev.sh ] && source /opt/dev/dev.sh
 
-      [[ -f /opt/dev/sh/chruby/chruby.sh ]] && type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; }
+      [[ -f /opt/dev/sh/chruby/chruby.sh ]] && { type chruby >/dev/null 2>&1 || chruby () { source /opt/dev/sh/chruby/chruby.sh; chruby "$@"; } }
 
       [[ -x /opt/homebrew/bin/brew ]] && eval $(/opt/homebrew/bin/brew shellenv)
 
@@ -250,6 +250,9 @@ in
   };
 
   programs.gitui = {
+    enable = true;
+  };
+  programs.yazi = {
     enable = true;
   };
 } 
