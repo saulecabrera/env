f39f789d7bb56201223c5ecc81b3636825e9979c
Saúl Cabrera <saulecabrera@gmail.com>
2025-04-28 08:49:49 -0400

    Better aerospace configuration

--- modules/darwin/home.nix
+++ modules/darwin/home.nix
@@ -17,6 +17,7 @@
   home.file.".config/zed/keymap.json".text = builtins.readFile ../shared/zed-keymap.json;
   home.file.".config/zed/settings.json".text = builtins.readFile ../shared/zed-settings.json;
   home.file.".config/ghostty/config".text = builtins.readFile ../shared/ghostty-config;
+  home.file.".aerospace.toml".text = builtins.readFile ./aerospace.toml;
 
   programs.home-manager.enable = true;
 }
